b86781108a82da52cc6c91aa6668886cd824a5b8
Tuan Anh Le <leanhtuanptithcm@gmail.com>
2023-02-03 15:09:07 +0700

    chore: update env

---
+++ apps/mobile/lib/env/env.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _Env {
+  static const appName = 'DGG';
+  static const baseUrl = 'url';
+  static const lang = 'en';
+}
